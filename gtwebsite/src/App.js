d835236a15cd8e13e8cb3fcbba007fc04946597a
Akim <jakim2621@gmail.com>
2023-11-21 17:50:14 +0100

    Adding Navbar To Website

--- gtwebsite/src/App.js
+++ gtwebsite/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import AppNavbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<AppNavbar />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
